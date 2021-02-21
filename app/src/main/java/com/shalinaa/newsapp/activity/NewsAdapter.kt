5e89cae8c892af1ce1b9dbd9c9aee357f81c7dc3
shalinaazd <ashalinaazhr@gmail.com>
2021-02-21 21:22:57 +0700

    update project

--- app/src/main/java/com/shalinaa/newsapp/activity/NewsAdapter.kt
+++ app/src/main/java/com/shalinaa/newsapp/activity/NewsAdapter.kt
@@ -1,6 +1,7 @@
 package com.shalinaa.newsapp.activity
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,16 +12,29 @@ import com.shalinaa.newsapp.model.ArticlesItem
 import com.shalinaa.newsapp.model.ResponseNews
 import kotlinx.android.synthetic.main.news_item.view.*
 
-class NewsAdapter (var context: Context, var listNews: List<ArticlesItem?>?)
-    : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+class NewsAdapter (var context: Context, var listNews: List<ArticlesItem?>?) :
+    RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-    class ViewHolder (view : View) :RecyclerView.ViewHolder(view){
+    private var onItemClickCallBack : OnItemClickCallBack? = null
+
+    fun setItemOnClickCallBack (onItemClickCallBack: OnItemClickCallBack){
+        this.onItemClickCallBack = onItemClickCallBack
+    }
+
+    interface OnItemClickCallBack {
+        fun onItemClicked(newsData : ArticlesItem)
+    }
+
+    inner class ViewHolder (view : View) :RecyclerView.ViewHolder(view){
         fun bind (news: ArticlesItem){
             with(itemView){
                 tv_title_item.text = news.title
                 tv_date_item.text = news.publishedAt
                 tv_duration_item.text = news.author
                 Glide.with(context).load(news.urlToImage).centerCrop().into(iv_item_news)
+                itemView.setOnClickListener{
+                    onItemClickCallBack?.onItemClicked(news)
+                }
             }
         }
     }
@@ -32,6 +46,12 @@ class NewsAdapter (var context: Context, var listNews: List<ArticlesItem?>?)
 
     override fun getItemCount(): Int = listNews!!.size
 
-    override fun onBindViewHolder(holder: NewsAdapter.ViewHolder, position: Int) =
-            holder.bind(listNews?.get(position)!!)
+    override fun onBindViewHolder(holder: NewsAdapter.ViewHolder, position: Int) {
+        holder.bind(listNews?.get(position)!!)
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.EXTRA_NEWS, listNews!![position])
+        }
+    }
+
 }
